0ba60d5d52e16f504949ac8e5d0ca49be2488b06
fathan <sansfathan@gmail.com>
2023-09-13 13:32:49 +0700

    pull-repository

--- src/Api/url.js
+++ src/Api/url.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const client = axios.create({
-  baseURL: "http://localhost:60",
+  baseURL: "http://172.16.80.16",
 
   headers: {
     Accept: "application/json",
