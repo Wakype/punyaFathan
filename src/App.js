d2bbbe35f67d5f0c278ecf5851e283affa8f3662
fathan <sansfathan@gmail.com>
2023-10-05 10:37:46 +0700

    masih-tahap-beta

--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ import {
   Navigate,
 } from "react-router-dom";
 import "./App.css";
-import Login from "./page/auth/login";
-import Dashboard from "./page/dashboard/dashboard";
-
-import ForgotPassword from "./page/auth/forgotP";
-import ResetPassword from "./page/auth/resetP";
-import Report from "./page/dashboard/report";
-import Produk from "./page/dashboard/produk";
-import User from "./page/dashboard/user";
-import ProtectRoute from "./protect-route/protectRoute";
 
 function App() {
   return (
-    <></>
+    <div>
+      silakan isi sendiri ya !!!  😚😚😚
+    </div>
   );
 }
 
