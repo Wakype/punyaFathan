adbe66336474034bc0af2a0f62e0a5c003efdc9e
fathan <sansfathan@gmail.com>
2023-10-05 12:33:53 +0700

    masih-tahap-beta

--- src/App.js
+++ src/App.js
@@ -10,6 +10,73 @@ function App() {
   return (
     <div>
       silakan isi sendiri ya !!!  😚😚😚
+
+
+      {/* <Routes>
+      // Page Login
+      <Route path="/login" element={<Login />} />
+      <Route
+      // Page Reset Password
+        path="/auth/reset-password/:id/:token"
+        element={<ResetPassword />}
+      />
+      // Page Forgot password
+      <Route path="/forgotPassword" element={<ForgotPassword />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          // Page Dashboard 
+          <ProtectRoute>
+            <Dashboard />
+          </ProtectRoute>
+        }
+      />
+      <Route
+        path="/dashboard/User"
+        // Page User Crud
+        element={
+          <ProtectRoute>
+            <User />
+          </ProtectRoute>
+        }
+      />
+      <Route
+      // Page Laporan*
+        path="/dashboard/Report"
+        element={
+          <ProtectRoute>
+            <Report />
+          </ProtectRoute>
+        }
+      />
+      <Route
+      // Page produk + CRUD
+        path="/dashboard/Produk"
+        element={
+          <ProtectRoute>
+            <Produk />
+          </ProtectRoute>
+        }
+      />
+      <Route
+      // Page Profile
+        path="/profile" 
+        element={
+          <ProtectRoute>
+            <Profile />
+          </ProtectRoute>
+        }
+      />
+      <Route path="*" element={<Navigate to="/login" replace={true} />} />
+    </Routes> */}
+
+    // *note : itu laporan ada 3 page : 
+    // - laporan barang masuk
+    // - laporan stok habis 
+    // - laporan laba rugi (gw gak tau klo yang ini soalnya belum di kasih tau ama pak nur / pak ihsan) 
+    // intinya laba rugi itu (buat keuangan dari transaksi keuangan yang berjalan selama satu bulan atau satu tahun baik transaksi yang menghasilkan kerugian maupun laba,
+    // Akumulasi dari total finansial tersebut yang akan menjadi laporan laba rugi perusahaan di periode tertentu.) 
     </div>
   );
 }
