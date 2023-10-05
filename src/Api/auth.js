118e87b9f33417a292fe06c3b9b7fb6da238ace3
fathan <sansfathan@gmail.com>
2023-10-05 14:04:04 +0700

    masih-tahap-beta

---
+++ src/Api/auth.js
@@ -0,0 +1,26 @@
+import axios, { syncToken } from "./url";
+
+
+
+
+
+export async function login(payload) {
+  console.log("Berhasil Login  =>")
+  return axios.post("/auth/login", payload);
+}
+
+export async function registerProses(payload) {
+  console.log("Berhasil mengambil data =>")
+  return axios.post("/auth/register", payload);
+}
+export async function forgotPassword(payload) {
+  return axios.post("/auth/lupa-password", payload);
+}
+export async function resetPassword(payload) {
+  return axios.post("/auth/reset-password/:id/:token", payload);
+}
+
+export async function authMeProcess() {
+  syncToken()
+  return axios.get("/authMe");
+}
---
+++ src/Api/user.js
@@ -0,0 +1,40 @@
+import axios from "./url";
+
+
+export async function getUser(){
+    console.log("Berhasil mengambil data =>")
+    return axios.get("user/list")
+}
+
+export async function getDetailUser(id){
+    console.log("user id =>",id)
+    return axios.get(`user/detail/${id}`,{
+        headers: {
+            "Content-Type": "multipart/form-data",
+          },
+    })
+}
+
+export async function createUser(payload){
+    console.log('====================================');
+    console.log("berhasil create =>",payload);
+    console.log('====================================');
+    return axios.post('user/create',payload)
+}
+
+export async function updateUser(id){
+    return axios.put(`user/update/${id}`,{
+        headers: {
+            "Content-Type": "multipart/form-data",
+          },
+    })
+}
+
+export async function deleteUser(id){
+    console.log('====================================');
+    console.log('id dapet gk?', id);
+    console.log('====================================');
+    return axios.delete(`user/delete/${id}`)
+}
+
+
